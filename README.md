7a3d2caede49e60280f1d1265debaff681207431
Teru Yamazaki <tyamazaki@kazamiya.net>
2024-01-02 14:07:41 +0900

    Add: script for creating ISO with preseed.cfg

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ Run the following command.
 ```
 wget -O - https://raw.githubusercontent.com/4n6ist/DFI2/main/DFI2_setup.bash | bash
 ```
+
+## preseed_debian_iso_lxde.sh
+
+preseed_debian_iso_lxde.sh creates a custom Debian Install ISO with an preseed.cfg. It configures hostname as dfi2, user name as forensics, and install LXDE. 
+
+Get an official netinst ISO (debian-x.y.z-amd64-netinst) from official site (https://www.debian.org/download), then run the script on Debian based OS.
---
+++ preseed_debian_iso_lxde.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 debian-<ver>-<arch>-netinst.iso"
+    exit 1
+fi
+
+filename="$1"
+version=$(echo "$filename" | sed -n 's/.*-\([0-9.]\+\)-.*-netinst\.iso/\1/p')
+
+# Check if the version number extraction was successful
+if [ -z "$version" ]; then
+    echo "Failed to extract the version number. The provided filename may not match the expected format."
+else
+    echo "Extracted version number: $version"
+fi
+
+sudo apt-get install libarchive-tools genisoimage
+wget https://raw.githubusercontent.com/4n6ist/DFI2/main/preseed.cfg
+mkdir isofiles
+bsdtar -C ./isofiles -xf ${filename}
+chmod +w -R isofiles/install.amd/
+gunzip isofiles/install.amd/initrd.gz 
+echo preseed.cfg | cpio -H newc -o -A -F isofiles/install.amd/initrd
+gzip isofiles/install.amd/initrd 
+chmod -w -R isofiles/install.amd/
+cd isofiles/
+chmod +w md5sum.txt 
+find -follow -type f ! -name md5sum.txt -print0 | xargs -0 md5sum > md5sum.txt 
+chmod -w md5sum.txt 
+cd ..
+sudo enisoimage -r -J -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o preseed-${filename} isofiles
+sudo rm -rf isofiles preseed.cfg
+echo "Completed creating preseed-${filename}"
